19ae8033e23a0d52be8c809cdba7e0f3fe863800
Xavier Guzman <guzmanxavi112@gmail.com>
2021-03-02 21:58:38 -0600

    added api route to create workouts

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,6 +1,19 @@
 const db = require("../models");
 const app = require("express").Router();
 
+// creates the workout
+app.post("/api/workout", ({ body }, res) => {
+  // console.log("workout");
+  db.workout
+    .create(body)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 // gets the workout and totals the duration
 app.get("/api/workouts", (req, res) => {
   db.Workout.find({}).then((dbWorkout) => {
